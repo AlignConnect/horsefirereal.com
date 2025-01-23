64d0493121d9f2817ebfb002d134f81c5e0393eb
AlignConnect <devlopersalignconnect@gmail.com>
2025-01-23 12:42:22 +0530

    latest code

--- app/layout.js
+++ app/layout.js
@@ -43,8 +43,24 @@ export default function RootLayout({ children }) {
         `}
       </Script>
 
+      <Script strategy="lazyOnload">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W7N2XRZM');
+        `}
+      </Script>
+
       <body className={inter.className}>
-        
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-W7N2XRZM"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+
         {/* START ExoClick Goal Tag | Website Interation  */}
         <script
           type="application/javascript"
